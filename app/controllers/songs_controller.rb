8381f3e72b650dd02753a06d741c6ac35c3c0fa2
Carlos Cabrera <ccm.contacto2@gmail.com>
2019-06-06 16:34:01 -0400

    Se agregaron links a los index de usario y playlist

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,12 +3,17 @@ class SongsController < ApplicationController
        song = Song.new(song_params) 
        song.playlist = Playlist.find(params[:playlist_id])
        song.save
-       redirect_to playlist_path(song.playlist), notice: 'La canción se ha creado con éxito'
+       @playlist = @song.playlist
+       if @song.save
+        redirect_to playlist_path(@song.playlist), notice: 'La canción se ha creado con éxito'
+       else
+        render 'playlists/show'
+       end
     end 
     def destroy
         song = Song.find(params[:id])
         song.destroy
-        redirect_to playlist_path(song.playlist)
+        redirect_to playlist_path(song.playlist), notice: 'La canción fué eliminada con éxito'
     end
     private
     def song_params
